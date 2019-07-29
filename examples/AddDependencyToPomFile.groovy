f2c088a0d27d1a8e4a39afb97a1a7dd61c54e1e6
aistomin <andrej.istomin.ikeen@gmail.com>
2019-07-30 00:27:29 +0200

    Issue #64. AddDependencyToPomFile.groovy is fixed.

--- examples/AddDependencyToPomFile.groovy
+++ examples/AddDependencyToPomFile.groovy
@@ -8,12 +8,12 @@ import com.github.aistomin.maven.browser.MavenCentral
     @Grab(
         group = 'com.github.aistomin',
         module = 'maven-browser',
-        version = '10.0-SNAPSHOT'
+        version = '1.0'
     )
 )
-final slurper = new XmlSlurper()
+final slurper = new XmlSlurper(false, false)
 println('Parsing POM file .....')
-final pomRoot = new XmlSlurper().parseText(new File('pom_sample.xml').text)
+final pomRoot = slurper.parseText(new File('pom_sample.xml').text)
 println()
 println('Find Maven artifact com.github.aistomin:jenkins-sdk:0.0.2 .....')
 final repository = new MavenCentral()
@@ -23,7 +23,7 @@ final version = repository.findVersions(
 ).find { '0.0.2' == it.name() }
 println()
 println("Add jenkins-sdk dependency to the POM file.")
-pomRoot.find { it.name() == 'dependencies'}
+pomRoot.children().find { it.name() == 'dependencies' }
     .appendNode(slurper.parseText(version.dependency().forMaven()))
 println()
 println("Save modified POM file as pom_sample_modified.xml .....")

4ef2fa9d7036bd7342b35d7361753d9cd3178b9e
aistomin <andrej.istomin.ikeen@gmail.com>
2022-07-30 00:59:15 +0200

    Issue #227. version 2.2 is ready to be released.

--- examples/ReadingRepoExample.groovy
+++ examples/ReadingRepoExample.groovy
@@ -7,7 +7,7 @@ import com.github.aistomin.maven.browser.MavenCentral
     @Grab(
         group = 'com.github.aistomin',
         module = 'maven-browser',
-        version = '2.1'
+        version = '2.2'
     )
 )
 

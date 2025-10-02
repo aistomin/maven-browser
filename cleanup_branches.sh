1b3bd3dca7d06f0fc776c9e41000b2570acba06b
aistomin <andrej.istomin.ikeen@gmail.com>
2025-10-02 11:36:41 +0200

    Issue #450. cleanup_branches.sh script is fixed.

--- cleanup_branches.sh
+++ cleanup_branches.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
-git fetch --prune; git branch -vv | egrep -v "(\[origin\/[a-zA-Z0-9/_-]+\])" | awk "{print \$1}" | xargs git branch -D
 
+# Get current branch name
+current_branch=$(git branch --show-current)
+
+# Fetch latest changes and prune deleted remote branches
+git fetch --prune
+
+# Find local branches that don't have corresponding remote branches
+# (either no tracking info or tracking a deleted remote branch)
+# and exclude the current branch from deletion
+git branch -vv | \
+  awk '{
+    # Skip the current branch (starts with *)
+    if ($1 == "*") next
+    
+    # Check if branch has no tracking info or tracking a deleted remote branch
+    # For non-current branches: $1=branch_name, $2=commit, $3=tracking_info
+    # For current branch: $1=*, $2=branch_name, $3=commit, $4=tracking_info
+    tracking_info = $3 " " $4
+    if (tracking_info == "" || tracking_info ~ /: gone/) {
+      print $1
+    }
+  }' | \
+  grep -v "^$current_branch$" | \
+  xargs -r git branch -D
+
+echo "Cleanup completed. Current branch is '$current_branch'."
